32b8e4792d53e24f98a6699260cd997d1372c3d2
Judalm Park (dami) <wnekfa1004@naver.com>
2023-03-18 20:30:15 +0900

    유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -54,8 +55,14 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
     @Component("helloBean")
-    static  class HelloBean{
+    static class HelloBean {
         public String hello(String data) {
             return "Hello" + data;
         }
